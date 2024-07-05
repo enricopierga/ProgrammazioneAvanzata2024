53d62ea19341d8c7f62bf67321711f30b7b7fad6
davidedg11 <155651176+davidedg11@users.noreply.github.com>
2024-07-05 19:04:58 +0200

    Delete backend2 directory

--- backend2/node2/src/routes/PaymentRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { effettuaPagamento } from '../controllers/PaymentController';
-
-const router = Router();
-
-router.post('/paga', effettuaPagamento);
-
-export default router;
