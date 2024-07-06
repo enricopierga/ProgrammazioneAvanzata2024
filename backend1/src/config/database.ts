552887f69ed2df48aa4217a0abf93c77d7689406
davidedg11 <155651176+davidedg11@users.noreply.github.com>
2024-07-06 17:37:18 +0200

    Delete backend1 directory

--- backend1/src/config/database.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: '../../data/transits.sqlite'  // Risale di due cartelle rispetto a config/database.ts
-});
-
-export default sequelize;
