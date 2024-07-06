007cbd784b97459d0f881e3a3b1789ad8e25456d
enricopierga <S1093538@studenti.univpm.it>
2024-07-06 15:47:43 +0200

    routes backend2

--- backend2/node2/src/routes/PaymentRoutes.ts
+++ backend2/node2/src/routes/PaymentRoutes.ts
@@ -1,10 +1,15 @@
-import { Router } from 'express';
-import { effettuaPagamento } from '../controllers/pagamentoController';
-import { authenticateJWT, authorizeAutomobilista } from '../middleware/role';
+import { Router } from "express";
+import { effettuaPagamento } from "../controllers/PaymentController";
+import { authenticateJWT, authorizeAutomobilista } from "../middleware/roles";
 
 const router = Router();
 
 // Rotta per effettuare un pagamento, accessibile solo agli automobilisti
-router.post('/paga', authenticateJWT, authorizeAutomobilista, effettuaPagamento);
+router.post(
+	"/paga",
+	authenticateJWT,
+	authorizeAutomobilista,
+	effettuaPagamento
+);
 
 export default router;
