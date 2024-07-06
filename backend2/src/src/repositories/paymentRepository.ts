051877c4372c3a3d5af4930923b51c290d04dd19
enricopierga <S1093538@studenti.univpm.it>
2024-07-06 16:12:22 +0200

    backend2 backup

--- backend2/src/src/repositories/paymentRepository.ts
+++
@@ -1,9 +0,0 @@
-import Pagamento from '../models/PaymentModel';
-
-class PagamentoRepository {
-  public async createPagamento(uuidPagamento: string, importo: number, utenteId: number): Promise<Pagamento> {
-    return Pagamento.create({ uuidPagamento, importo, utenteId });
-  }
-}
-
-export default new PagamentoRepository();
