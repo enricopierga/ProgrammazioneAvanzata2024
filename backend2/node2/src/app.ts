c6c71a088260502c3634d011d9c4d5515075c3b5
enricopierga <S1093538@studenti.univpm.it>
2024-07-06 15:50:07 +0200

    tsconfig backend2

--- backend2/node2/src/app.ts
+++ backend2/node2/src/app.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import sequelize from './config/database';
-import utenteRoutes from './routes/UserRoutes';
-import pagamentoRoutes from './routes/PaymentRoutes';
-import dotenv from 'dotenv';
-import { errorHandler } from './middleware/errorHandler';
+import express from "express";
+import sequelize from "./config/database";
+import utenteRoutes from "./routes/UserRoutes";
+import pagamentoRoutes from "./routes/PaymentRoutes";
+import dotenv from "dotenv";
+import { errorHandler } from "./middleware/errorHandler";
 
 dotenv.config();
 
@@ -13,18 +13,19 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Usa le rotte
-app.use('/utente', utenteRoutes);
-app.use('/pagamento', pagamentoRoutes);
+app.use("/utente", utenteRoutes);
+app.use("/pagamento", pagamentoRoutes);
 
 // Middleware per la gestione degli errori
 app.use(errorHandler);
 
+// Funzione di avvio del server
 app.listen(port, async () => {
-  console.log(`Server is running on port ${port}`);
-  try {
-    await sequelize.sync();
-    console.log('Database connected successfully');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+	console.log(`Server is running on port ${port}`);
+	try {
+		await sequelize.sync();
+		console.log("Database connected successfully");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
 });
