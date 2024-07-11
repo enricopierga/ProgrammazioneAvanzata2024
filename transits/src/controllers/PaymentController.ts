8acb800cc5757f24f5766c03d55b5f86014b8506
enricopierga <S1093538@studenti.univpm.it>
2024-07-11 18:54:19 +0200

    add files to transits backend

---
+++ transits/src/controllers/PaymentController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import InfractionRepository from "../repositories/InfractionRepository";
+
+class PaymentController {
+	async payInfractionByUuid(req: Request, res: Response): Promise<void> {
+		try {
+			const { uuidPayment } = req.body;
+
+			// Trova l'infrazione associata tramite UUID
+			const infraction = await InfractionRepository.getByUuid(
+				uuidPayment
+			);
+			if (!infraction) {
+				res.status(404).json({ message: "Infraction not found" });
+				return;
+			}
+
+			// Assicurarsi che l'infrazione non sia già stata pagata
+			if (infraction.paid) {
+				res.status(400).json({ message: "Fine already paid" });
+				return;
+			}
+
+			// Aggiorna lo stato dell'infrazione come pagata
+			infraction.paid = true;
+			await InfractionRepository.update(infraction.id, infraction);
+
+			res.status(200).json({
+				message: "Fine paid successfully",
+				infraction,
+			});
+		} catch (error) {
+			res.status(500).json({ message: "Internal server error", error });
+		}
+	}
+}
+
+export default new PaymentController();
---
+++ transits/src/routes/PaymentRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import PaymentController from "../controllers/PaymentController";
+import { requireAuthentication } from "../middleware/roles";
+
+const router = Router();
+
+// Rotta per effettuare un pagamento di una multa, accessibile solo agli automobilisti
+router.post(
+	"/paga",
+	requireAuthentication("Automobilista"),
+	PaymentController.payInfractionByUuid
+);
+
+export default router;
